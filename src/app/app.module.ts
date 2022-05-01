9efe2712e7b026e69306cb5d793a28ce05c6d035
Dennys J Marquez <dennysjmarquez@gmail.com>
2022-05-01 16:08:20 -0400

    + Implementación de Auth0 en el app.module.ts + Configurando el interceptor para que HttpClientModule inserte el token, en los headers, según la lista de direcciones para el interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,39 @@
-import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+
+import {AuthHttpInterceptor, AuthModule} from '@auth0/auth0-angular';
+import {environment as env} from '../environments/environment';
 
 // Modules
-import {AppRoutersModule} from "./routers/app-routers-routing.module";
+import {ComponentsModule} from './components/components.module';
+import {AppRoutersModule} from './routers/app-routers-routing.module';
 
 // Components
 import {AppComponent} from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutersModule
+    AppRoutersModule,
+    HttpClientModule,
+    AuthModule.forRoot({
+      ...env.auth0,
+      httpInterceptor: {
+        allowedList: [`${env.SERVER_URL}/api/movies`],
+      },
+    }),
+    ComponentsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHttpInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {
 }
