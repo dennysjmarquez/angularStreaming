9603b5686b05fe73fc46f3754b1bfb6880939b98
Dennys J Marquez <dennysjmarquez@gmail.com>
2022-05-01 20:04:50 -0400

    + Creación de la home y sus componentes

--- src/app/pages/public-zone/home/home.component.ts
+++ src/app/pages/public-zone/home/home.component.ts
@@ -1,23 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MoviesService } from '../../movies-zone/movies/services/movies.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private _router: Router, _moviesService: MoviesService) {}
 
-  constructor(private _router: Router,) {
-
-  }
-
-  ngOnInit(): void {
-
-  }
-
- async goMovies(){
-    await this._router.navigate(['/movies'])
-  }
-
+  ngOnInit(): void {}
 }
