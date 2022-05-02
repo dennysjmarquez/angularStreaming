cccc3970ca516983cfe2ffbbcec3b9c1c4f8ea50
Dennys J Marquez <dennysjmarquez@gmail.com>
2022-05-01 20:04:19 -0400

    + Creación de la página para mostrar las películas y series

--- src/app/pages/movies-zone/movies/movies.component.ts
+++ src/app/pages/movies-zone/movies/movies.component.ts
@@ -1,18 +1,30 @@
-import {Component, OnInit} from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  EntriesType,
+  MoviesService,
+  MoviesType,
+} from './services/movies.service';
+import { catchError, finalize } from 'rxjs';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.css']
+  styleUrls: ['./movies.component.css'],
 })
 export class MoviesComponent implements OnInit {
+  public movies: EntriesType[] = [];
 
-  constructor() {
+  constructor(private _moviesService: MoviesService) {
+    _moviesService
+      .getMovies()
+      .pipe(
+        catchError((err): any => {}),
+        finalize(() => {})
+      )
+      .subscribe((movies: MoviesType) => {
+        this.movies = movies.entries;
+      });
   }
 
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
