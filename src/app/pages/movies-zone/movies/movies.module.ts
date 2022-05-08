8f29ca888b09b39f5096ce326a9fa1f45fd462d1
Dennys J Marquez <dennysjmarquez@gmail.com>
2022-05-07 22:14:42 -0400

    Agregado del módulo ReactiveFormsModule para manejar las validaciones y el value del input del filtro año

--- src/app/pages/movies-zone/movies/movies.module.ts
+++ src/app/pages/movies-zone/movies/movies.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies.component';
-
+import { SortingButtonComponent } from './components/sorting-button/sorting-button.component';
+import { FilterYearComponent } from './components/filter-year/filter-year.component';
+import { InputOnlyNumbersDirective } from './directives/input-only-numbers.directive';
+import { FilterTypeComponent } from './components/filter-type/filter-type.component';
 
 @NgModule({
-  declarations: [
-    MoviesComponent
-  ],
-  imports: [
-    CommonModule,
-    MoviesRoutingModule
-  ]
+  declarations: [MoviesComponent, SortingButtonComponent, FilterYearComponent, InputOnlyNumbersDirective, FilterTypeComponent],
+  imports: [CommonModule, MoviesRoutingModule, ReactiveFormsModule],
 })
-export class MoviesModule { }
+export class MoviesModule {}
